50fc008ea9468376e9ec4ed658f3e72a671d9bb6
Iago Soriano <iagosoriano.labenu@gmail.com>
2022-08-02 11:04:48 -0300

    can talk to db

--- infra/staging/delete-billed-resources.sh
+++ infra/staging/delete-billed-resources.sh
@@ -2,3 +2,4 @@
 aws ecs update-service --cluster aws-monorepo-poc-ecs-cluster-staging --service aws-monorepo-poc-api-1-staging --desired-count 0
 aws ecs update-service --cluster aws-monorepo-poc-ecs-cluster-staging --service aws-monorepo-poc-api-2-staging --desired-count 0
 aws ec2 delete-nat-gateway --nat-gateway-id `aws ec2 describe-nat-gateways | python3 -c "import sys, json; print(json.load(sys.stdin)['NatGateways'][0]['NatGatewayId'])"`
+aws ec2 release-address --allocation-id `aws ec2 describe-addresses | python3 -c "import sys, json; print(json.load(sys.stdin)['Addresses'][0]['AllocationId'])"`
